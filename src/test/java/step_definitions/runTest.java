0ddcbf2d656242afb355e34ba1abfb50e616027b
dfzheng <dfzheng@thoughtworks.com>
2017-09-20 10:59:32 +0800

    add features

--- src/test/java/step_definitions/runTest.java
+++ src/test/java/step_definitions/runTest.java
@@ -4,6 +4,6 @@ import cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @Cucumber.Options(
 format={"pretty", "html:target/cucumber"},
-features = "features/QuestionJive2Email.feature")
+features = {"features/QuestionJive2Email.feature", "features/Email2Jive.feature"})
 public class runTest {
 }
